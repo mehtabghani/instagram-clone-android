2a2395cd85e2128b2ab729731fc64e4ed83da1be
mehtab <mehtab.ghani@gmail.com>
2018-09-22 20:29:15 +0500

    login and reciter activity UI have been created.

--- app/src/main/java/com/maddy/instagramclone/activity/LikesActivity.java
+++ app/src/main/java/com/maddy/instagramclone/activity/LikesActivity.java
@@ -17,8 +17,8 @@ public class LikesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_likes);
-        setupBottomNavigationView();
+        setContentView(R.layout.activity_register);
+        //setupBottomNavigationView();
     }
 
     private void setupBottomNavigationView() {
